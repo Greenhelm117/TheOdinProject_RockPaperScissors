37578f9d6c951ca710739e0902abcb9c4a7cbe9c
Greenhelm <andrew.b.sheils@gmail.com>
2022-12-11 08:47:01 -0500

    Add buttons to html file

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -16,6 +16,10 @@ const rock = "rock";
 const paper = "paper";
 const scissors = "scissors";
 
+
+
+
+
 function getWinner()
 {
 
@@ -46,32 +50,185 @@ function getComputerChoice()
 
 function getPlayerChoice()
 {
+    let theChoice = prompt("Choose rock, paper, or scissors.")
 
+    theChoice = theChoice.toLowerCase();
 
-    return rock;
+    if (theChoice === rock || theChoice === paper || theChoice === scissors)
+    {
+        return theChoice;
+    }
+    else
+
+    return "Error";
+
+    
 }
 
 
 function oneRound(computerChoice, playerChoice)
 {
-    let theWinner;
+    //let theWinner;
 
     computerChoice = getComputerChoice();
-    playerChoice = getPlayerChoice()
+    playerChoice = getPlayerChoice();
 
-    if (playerChoice = rock && computerChoice = rock)
+    computerWins = 0;
+    playerWins = 0;
+
+   /* if (playerChoice = rock && computerChoice == rock)
     {
-        return "Tie"
+        return "Tie";
     }
-    else if (playerChoice = paper && computerChoice = paper)
+    else if (playerChoice = paper && computerChoice == paper)
     {
-        return 
+        return "Tie";
     }
+    else if (playerChoice = scissors && computerChoice == scissors)
+    {
+        return "Tie";
+    } */
+
+    console.log(playerChoice);
+    console.log(computerChoice);
+
+    if (playerChoice == rock && computerChoice == paper)
+    {
+        computerWins = 1;
+        playerWins = 0;
+
+        return "Computer wins! Paper beats rock!";
 
 
-    return theWinner;
+
+    }
+    else if (playerChoice == rock && computerChoice == scissors)
+    {
+
+        computerWins = 0;
+        playerWins = 1;
+
+        return "You win! Rock beats scissors!";
+
+    }
+    else if (playerChoice == rock && computerChoice == rock)
+    {
+
+        computerWins = 0;
+        playerWins = 0;
+
+        return "Tie";
+
+   
+    }
+    else if (playerChoice == paper && computerChoice == scissors)
+    {
+        computerWins = 1;
+        playerWins = 0;
+        return "Computer wins! Scissors beats paper!";
+
+
+    }
+    else if (playerChoice == paper && computerChoice == rock)
+    {
+
+        computerWins = 0;
+        playerWins = 1;
+
+        return "You win! Paper beats rock!";
+
+  
+    }
+    else if (playerChoice == paper && computerChoice == paper)
+    {
+        computerWins = 0;
+        playerWins = 0;
+        return "Tie";
+    }
+    else if (playerChoice == scissors && computerChoice == paper)
+    {
+        computerWins = 0;
+        playerWins = 1;
+
+        return "You win! Scissors beats paper!";
+
+    }
+    else if (playerChoice == scissors && computerChoice == rock)
+    {
+
+        computerWins = 1;
+        playerWins = 0;
+
+        return "Computer wins! Rock beats scissors!";
+  
+    }
+    else if (playerChoice == scissors && computerChoice == scissors)
+    {
+        computerWins = 0;
+        playerWins = 0;
+
+        return "Tie";
+    }
+    else
+
+
+
+    return "Error";
 }
 
 
+/*function Game()
+{
+    let Winner = "no one";
+    computerScore = 0;
+    playerScore = 0;
 
-console.log(oneRound())
+    for(i = 0; i < 5; i++ )
+    {
+
+
+        oneRound();
+
+        if (playerWins == 1)
+        {
+            playerScore++;
+        }
+        else if (computerWins == 1)
+        {
+            computerScore++;
+        }
+
+
+
+
+
+
+    }
+
+    if(playerScore > computerScore)
+    {
+        Winner = "Player wins!";
+        return Winner + playerScore + "to" + computerScore;
+    }
+    else if(computerScore > playerScore)
+    {
+        Winner = "Computer wins!";
+        return Winner + computerScore + "to" + playerScore;
+    }
+    else
+    {
+        Winner = "Tie!";
+        return Winner + computerScore + "to" + playerScore;;
+    }
+
+
+    
+
+} */
+
+
+
+console.log(Game());
+
+//console.log(getPlayerChoice())
+//console.log(getComputerChoice())
